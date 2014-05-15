33b72666911a757cbecc96639eef9b472ba7d1b9
cpmatthew <matthew.vetter@cpanel.net>
2014-05-15 02:51:42 -0500

    code cleanup

--- cpanel_preinstall_check.sh
+++ cpanel_preinstall_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###############################
 ##  cPanel Preinstall Check  ##
-##  Version 1.2.0.2          ##
+##  Version 1.2.0.4          ##
 ##  By: Matthew Vetter       ##
 ##      cPanel, Inc.         ##
 ###############################
@@ -21,7 +21,7 @@ if perl < /dev/null > /dev/null 2>&1  ; then
         echo -e "${green}Perl is Installed. Nothing to Fix!${NC}"
     else
         echo -e "${red}Perl not Installed. You need to install this!${NC}";
-        echo -e "     \_ To install perl run: yum install perl";
+        echo -e "\t \_ To install perl run: yum install perl";
 fi
 
 #Check SELINUX Status
@@ -33,15 +33,15 @@ selinuxfile="/etc/selinux/config"
 if [ -f "$selinuxfile" ] ; then
     if ``cat "$selinuxfile" | grep "#SELINUX=" > /dev/null`` ; then
             echo -e "${red}SELINUX is commented out! You need to uncomment this (remove the # from in front of SELINUX) and set this to disabled!${NC}";
-            echo -e "     \_ To fix this please review the following article http://www.cyberciti.biz/faq/howto-turn-off-selinux/ and apply the permanent fix by editing /etc/sysconfig/selinux and rebooting the server";
+            echo -e "\t \_ To fix this please review the following article http://www.cyberciti.biz/faq/howto-turn-off-selinux/ and apply the permanent fix by editing /etc/sysconfig/selinux and rebooting the server";
 elif [ -f "$selinuxfile" ] ; then
     if ``cat "$selinuxfile" | grep "SELINUX=" | grep "enforcing" > /dev/null`` ; then
             echo -e "${red}SELINUX is set to enforcing! You need to set this to disabled!${NC}";
-            echo -e "     \_ To fix this please review the following article http://www.cyberciti.biz/faq/howto-turn-off-selinux/ and apply the permanent fix by editing /etc/sysconfig/selinux and rebooting the server";
+            echo -e "\t \_ To fix this please review the following article http://www.cyberciti.biz/faq/howto-turn-off-selinux/ and apply the permanent fix by editing /etc/sysconfig/selinux and rebooting the server";
 elif [ -f "$selinuxfile" ] ; then
     if ``cat "$selinuxfile" | grep "SELINUX=" | grep "permissive" > /dev/null`` ; then
             echo -e "${red}SELINUX is set to permissive! You need to set this to disabled!${NC}";
-            echo -e "     \_ To fix this please review the following article http://www.cyberciti.biz/faq/howto-turn-off-selinux/ and apply the permanent fix by editing /etc/sysconfig/selinux and rebooting the server";
+            echo -e "\t \_ To fix this please review the following article http://www.cyberciti.biz/faq/howto-turn-off-selinux/ and apply the permanent fix by editing /etc/sysconfig/selinux and rebooting the server";
 else
     echo -e "${green}Nothing to Fix. SELINUX appears to be disabled already!${NC}"
 fi
@@ -56,13 +56,13 @@ if ``chkconfig --list | grep iptables | grep "0:off	1:off	2:off	3:off	4:off	5:of
         echo -e "${green}Firewall off in ChkConfig. Nothing to Fix!${NC}";
     elif ``chkconfig --list | grep iptables | grep "on" > /dev/null`` ; then
         echo -e "${red}Firewall enabled in ChkConfig. You should turn this off.${NC}";
-        echo -e "     \_ To turn this off run: chkconfig iptables off";
+        echo -e "\t \_ To turn this off run: chkconfig iptables off";
 fi
 
 # Check if Firewall Running
 if ``/etc/init.d/iptables status | grep "Table: filter" > /dev/null`` ; then
         echo -e "${red}Firewall Running. You should disable this.${NC}";
-        echo -e "     \_ To disable this run: /etc/init.d/iptables save; /etc/init.d/iptables stop";
+        echo -e "\t \_ To disable this run: /etc/init.d/iptables save; /etc/init.d/iptables stop";
     elif ``/etc/init.d/iptables status | grep "Firewall is not running" > /dev/null`` ; then
         echo -e "${green}Firewall Not Running. Nothing to Fix!${NC}";
 fi
@@ -73,21 +73,21 @@ echo -e "${yellow}=====YUM GROUPS CHECK=====${NC}";
 
 if ``echo -e "n" | yum groupremove "FTP Server" | grep "Removing:" > /dev/null`` ; then
         echo -e "${red}FTP Server is Installed. You should remove this${NC}";
-        echo -e '     \_ To remove this run: yum groupremove "FTP Server"';
+        echo -e '\t \_ To remove this run: yum groupremove "FTP Server"';
     else
         echo -e "${green}FTP Server - Fixed${NC}";
 fi
 
 if ``echo -e "n" | yum groupremove "Web Server" | grep "Removing:" > /dev/null`` ; then
         echo -e "${red}Web Server is Installed. You should remove this${NC}";
-        echo -e '     \_ To remove this run: yum groupremove "Web Server"';
+        echo -e '\t \_ To remove this run: yum groupremove "Web Server"';
     else
         echo -e "${green}Web Server - Fixed${NC}";
 fi
 
 if ``echo -e "n" | yum groupremove "X Window System" | grep "Removing:" > /dev/null`` ; then
         echo -e "${red}X Window System is Installed. You should remove this${NC}";
-        echo -e '     \_ To remove this run: yum groupremove "X Window System"';
+        echo -e '\t \_ To remove this run: yum groupremove "X Window System"';
     else
         echo -e "${green}X Window System - Fixed${NC}";
 fi
@@ -97,21 +97,21 @@ if ``cat /etc/redhat-release | grep "release 6.*" > /dev/null``  ; then
 
     if ``echo -e "n" | yum groupremove "E-mail Server" | grep "Removing:"  > /dev/null`` ; then
             echo -e "${red}E-mail Server is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "E-mail Server"';
+            echo -e '\t \_ To remove this run: yum groupremove "E-mail Server"';
         else
             echo -e "${green}E-Mail Server - Fixed${NC}";
     fi
 
     if ``echo -e "n" | yum groupremove "KDE Desktop" | grep "Removing:" > /dev/null`` ; then
             echo -e "${red}KDE Desktop is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "KDE Desktop"';
+            echo -e '\t \_ To remove this run: yum groupremove "KDE Desktop"';
         else
             echo -e "${green}KDE Desktop - Fixed${NC}";
     fi
 
     if ``echo -e "n" | yum groupremove "Desktop" | grep "Removing:" > /dev/null`` ; then
             echo -e "${red}Gnome Desktop is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "Desktop"';
+            echo -e '\t \_ To remove this run: yum groupremove "Desktop"';
         else
             echo -e "${green}Gnome Desktop - Fixed${NC}";
     fi
@@ -124,28 +124,28 @@ if ``cat /etc/redhat-release | grep "release 5.*" > /dev/null``  ; then
 
     if ``echo -e "n" | yum groupremove "Mail Server" | grep "Removing:" | grep -v "No group named" > /dev/null`` ; then
             echo -e "${red}Mail Server is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "Mail Server"';
+            echo -e '\t \_ To remove this run: yum groupremove "Mail Server"';
         else
             echo -e "${green}Mail Server - Fixed${NC}";
     fi
 
     if ``echo -e "n" | yum groupremove "GNOME Desktop Environment" | grep "Removing:" > /dev/null`` ; then
             echo -e "${red}GNOME Desktop Environment is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "GNOME Desktop Environment"';
+            echo -e '\t \_ To remove this run: yum groupremove "GNOME Desktop Environment"';
         else
             echo -e "${green}GNOME Desktop Environment - Fixed${NC}";
     fi
 
     if ``echo -e "n" | yum groupremove "KDE (K Desktop Environment)" | grep "Removing:" > /dev/null`` ; then
             echo -e "${red}KDE (K Desktop Environment) is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "KDE (K Desktop Environment)"';
+            echo -e '\t \_ To remove this run: yum groupremove "KDE (K Desktop Environment)"';
         else
             echo -e "${green}KDE (K Desktop Environment) - Fixed${NC}";
     fi
 
     if ``echo -e "n" | yum groupremove "Mono" | grep "Removing:" > /dev/null`` ; then
             echo -e "${red}Mono is Installed. You should remove this${NC}";
-            echo -e '     \_ To remove this run: yum groupremove "Mono"';
+            echo -e '\t \_ To remove this run: yum groupremove "Mono"';
         else
             echo -e "${green}Mono - Fixed${NC}";
     fi
@@ -176,32 +176,29 @@ if [ $hostnameip != '0' ]; then
 
 if [ ! -z "$digresult" ]; then
     if [ $digresult == $hostnameip ]; then
-        echo -e "     \_ ${green}The IP the hostname resolves to is the same as what's set on the server${NC}"
+        echo -e "\t \_ ${green}The IP the hostname resolves to is the same as what's set on the server${NC}"
     else
-        echo -e "     \_ ${red}The hostname resolves to a different IP than what's set on the server${NC}"
-        echo -e "     \_ ${red}The hostname should resolve to $hostnameip, but actually resolves to $digresult${NC}"
+        echo -e "\t \_ ${red}The hostname resolves to a different IP than what's set on the server${NC}"
+        echo -e "\t \_ ${red}The hostname should resolve to $hostnameip, but actually resolves to $digresult${NC}"
 fi
     else
-        echo -e "     \_ ${red}The hostname on the server does not resolve to an IP address${NC}"
+        echo -e "\t \_ ${red}The hostname on the server does not resolve to an IP address${NC}"
 fi
     else
-        echo -e "     \_ ${red}The server's hostname is not in /etc/hosts!${NC}"
+        echo -e "\t \_ ${red}The server's hostname is not in /etc/hosts!${NC}"
 fi
 
 # OS Check
-
-MYOS=$(cat /etc/redhat-release)
-echo -e "     \_$OUT";
 
 echo -e "${yellow}=====SUPPORTED OS CHECK=====${NC}"
 
 if ``cat /etc/redhat-release | grep "release 5.*" > /dev/null``  ; then
             echo -e "${green}The OS is Supported${NC}";
-            echo -e "     \_ $MYOS";
+            echo -e "\t \_`cat /etc/redhat-release`"
 elif ``cat /etc/redhat-release | grep "release 6.*" > /dev/null``  ; then
             echo -e "${green}The OS is Supported${NC}";
-            echo -e "     \_ $MYOS";
+            echo -e "\t \_`cat /etc/redhat-release`"
 else
         echo -e "${red}The OS is Not Supported${NC}";
-        echo -e "     \_ $MYOS";
+        echo -e "\t \_`cat /etc/redhat-release`"
 fi
