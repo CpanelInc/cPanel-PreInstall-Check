c0902f9d02938711d2722d041584750473dd352b
cpmatthew <matthew.vetter@cpanel.net>
2014-07-22 04:52:14 -0500

    added switch for help menu, to disable color, & improved PASS/FAIL output

--- cpanel_preinstall_check.sh
+++ cpanel_preinstall_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###############################
 ##  cPanel Preinstall Check  ##
-##  Version 1.2.8.2          ##
+##  Version 1.2.9            ##
 ##  By: Matthew Vetter       ##
 ##      cPanel, Inc.         ##
 ###############################
@@ -11,14 +11,51 @@ green='\e[0;32m'
 red='\e[0;31m'
 yellow='\e[0;33m'
 NC='\e[0m' # No Color (if Not added, it will change the entire terminal output to the last color used)
-#example echo -e "${green}Show in Green${NC}"
+#example echo -e "${green}[PASS] * Show in Green${NC}"
+
+# Execute getopt on the arguments passed to this program, identified by the special character $@
+PARSED_OPTIONS=$(getopt -n "$0"  -o hn --long "help,nocolor"  -- "$@")
+
+#Bad arguments, something has gone wrong with the getopt command.
+if [ $? -ne 0 ];
+then
+  exit 1
+fi
+
+# A little magic, necessary when using getopt.
+eval set -- "$PARSED_OPTIONS"
+
+# Now goes through all the options with a case and using shift to analyse 1 argument at a time.
+#$1 identifies the first argument, and when we use shift we discard the first argument, so $2 becomes $1 and goes again through the case.
+while true;
+do
+    case "$1" in
+
+    -h|--help)
+        echo "-h / --help : Displays this screen";
+        echo "-n / --nocolor : Runs Script without Color Output";
+        exit 1;
+    shift;;
+
+    -n|--nocolor)
+        #disables color output
+        green='\e[0m'
+        red='\e[0m'
+        yellow='\e[0m'
+        NC='\e[0m'
+    shift;;
+
+    --)
+    shift
+    break;;
+    esac
+done
 
 #Check if cPanel Install Present
-
-echo -e "${yellow}=====cPanel Install CHECK=====${NC}";
+echo -e "====== cPanel Install CHECK ======";
 
 if ls /usr/local/cpanel >/dev/null 2>&1 || ls /var/cpanel >/dev/null 2>&1 || command -v /etc/init.d/cpanel >/dev/null 2>&1 ; then
-    echo -e "${red}cPanel Installation Found - Fail${NC}"
+    echo -e "${red}[FAIL] * cPanel Installation Found${NC}"
     if ls /usr/local/cpanel >/dev/null 2>&1; then
         echo -e "\t \_ cPanel/WHM `cat /usr/local/cpanel/version` Install Detected"
         echo -e "\t \_ You will need to start with a fresh OS install / reinstall the OS before installing cPanel"
@@ -27,15 +64,14 @@ if ls /usr/local/cpanel >/dev/null 2>&1 || ls /var/cpanel >/dev/null 2>&1 || com
         echo -e "\t \_ You will need to start with a fresh OS install / reinstall the OS before installing cPanel"
     fi
 else
-    echo -e "${green}cPanel not Installed - Pass${NC}"
+    echo -e "${green}[PASS] * cPanel not Installed${NC}"
 fi
 
 #Check if Perl Installed
-
-echo -e "${yellow}=====PERL CHECK=====${NC}";
+echo -e "====== PERL CHECK ======";
 
 if perl < /dev/null > /dev/null 2>&1  ; then
-    echo -e "${green}Perl is Installed - Pass${NC}";
+    echo -e "${green}[PASS] * Perl is Installed${NC}";
     echo -e "\t \_ Perl `perl -v | grep 'This is perl, v'| awk '{print $4}'` Installed - Verify this is a Supported Version";
     if `grep "release 6.*" /etc/redhat-release > /dev/null`  ; then
         echo -e "\t \_ Latest version in Yum is - Perl `yum info perl | grep Version | awk '{print $3}'`";
@@ -43,31 +79,29 @@ if perl < /dev/null > /dev/null 2>&1  ; then
         echo -e "\t \_ Latest version in Yum is - Perl ` yum info perl | awk '/Installed Packages/ {flag=1;next} /Available Packages/{flag=0} flag {print}' | grep Version | awk '{print $2}'`";
     fi
 else
-    echo -e "${red}Perl not Installed - Fail${NC}";
+    echo -e "${red}[FAIL] * Perl not Installed${NC}";
     echo -e "\t \_ To install perl run: yum install perl";
 fi
 
 #Check if wget Installed
-
-echo -e "${yellow}=====WGET CHECK=====${NC}";
+echo -e "====== WGET CHECK ======";
 
 if command -v wget >/dev/null 2>&1  ; then
-    echo -e "${green}wget is Installed - Pass${NC}";
+    echo -e "${green}[PASS] * wget is Installed${NC}";
 else
-    echo -e "${red}wget not Installed - Fail${NC}";
+    echo -e "${red}[FAIL] * wget not Installed${NC}";
     echo -e "\t \_ To install wget run: yum install wget";
 fi
 
 #Check SELINUX Status
-
-echo -e "${yellow}=====SELINUX CHECK=====${NC}";
+echo -e "====== SELINUX  CHECK ======";
 
 if `grep "#SELINUX=" /etc/selinux/config > /dev/null` ; then
-    echo -e "${red}SELINUX - Fail${NC}";
+    echo -e "${red}[FAIL] * SELINUX${NC}";
     echo -e "\t \_ SELINUX is commented out!"
     echo -e "\t \_ To fix this edit /etc/sysconfig/selinux, uncomment SELINUX= (remove the # from in front of SELINUX), sett it to disabled and then reboot the server";
 elif `sestatus | grep "enabled" > /dev/null` ; then 
-    echo -e "${red}SELINUX - Fail${NC}";
+    echo -e "${red}[FAIL] * SELINUX${NC}";
     echo -e "\t \_ Selinux is Enabled and is currently set to `sestatus | grep 'Current mode' | awk '{print $3}'`";
 
     if `sestatus | grep "Mode from config file:" | grep "disabled" > /dev/null`; then 
@@ -76,57 +110,56 @@ elif `sestatus | grep "enabled" > /dev/null` ; then
         echo -e "\t \_ To fix this edit /etc/sysconfig/selinux, set SELINUX= to disabled and then reboot the server";     
     fi
 elif `sestatus | grep "disabled" > /dev/null` ; then 
-    echo -e "${green}SELINUX - Pass${NC}";
+    echo -e "${green}[PASS] * SELINUX${NC}";
     echo -e "\t \_ SELINUX appears to be disabled already";
 fi
 
 # Check for Yum Groups All Versions (Group names the same accross all versions)
-
-echo -e "${yellow}=====YUM GROUPS CHECK=====${NC}";
+echo -e "====== YUM GROUPS CHECK ======";
 
 if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "FTP server" > /dev/null` ; then
-    echo -e "${red}FTP Server - Fail${NC}";
+    echo -e "${red}[FAIL] * FTP Server${NC}";
     echo -e '\t \_ To remove this run: yum groupremove "FTP Server"';
 else
-    echo -e "${green}FTP Server - Pass${NC}";
+    echo -e "${green}[PASS] * FTP Server${NC}";
 fi
 
 if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "Web Server" > /dev/null` ; then
-    echo -e "${red}Web Server - Fail${NC}";
+    echo -e "${red}[FAIL] * Web Server${NC}";
     echo -e '\t \_ To remove this run: yum groupremove "Web Server"';
 else
-    echo -e "${green}Web Server - Pass${NC}";
+    echo -e "${green}[PASS] * Web Server${NC}";
 fi
 
 if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "X Window System" > /dev/null` ; then
-    echo -e "${red}X Window System - Fail${NC}";
+    echo -e "${red}[FAIL] * X Window System${NC}";
     echo -e '\t \_ To remove this run: yum groupremove "X Window System"';
 else
-    echo -e "${green}X Window System - Pass${NC}";
+    echo -e "${green}[PASS] * X Window System${NC}";
 fi
 
 # New Group Names CentOS/RHEL 6.*
 if `grep "release 6.*" /etc/redhat-release > /dev/null`  ; then
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "E-mail server" > /dev/null` ; then
-        echo -e "${red}E-mail Server - Fail${NC}";
+        echo -e "${red}[FAIL] * E-mail Server${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "E-mail Server"';
     else
-        echo -e "${green}E-Mail Server - Pass${NC}";
+        echo -e "${green}[PASS] * E-Mail Server${NC}";
     fi
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "KDE Desktop" > /dev/null` ; then
-        echo -e "${red}KDE Desktop - Fail${NC}";
+        echo -e "${red}[FAIL] * KDE Desktop${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "KDE Desktop"';
     else
-        echo -e "${green}KDE Desktop - Pass${NC}";
+        echo -e "${green}[PASS] * KDE Desktop${NC}";
     fi
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "Desktop" > /dev/null` ; then
-        echo -e "${red}Gnome Desktop - Fail${NC}";
+        echo -e "${red}[FAIL] * Gnome Desktop${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "Desktop"';
     else
-        echo -e "${green}Gnome Desktop - Pass${NC}";
+        echo -e "${green}[PASS] * Gnome Desktop${NC}";
     fi
 
 elif `grep "release 5.*" /etc/redhat-release > /dev/null`  ; then
@@ -134,38 +167,37 @@ elif `grep "release 5.*" /etc/redhat-release > /dev/null`  ; then
     # Deprecated Group Names in CentOS/RHEL 6. Will check if on CentOS/RHEL 5.*
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "Mail Server" > /dev/null`; then
-        echo -e "${red}Mail Server - Fail${NC}";
+        echo -e "${red}[FAIL] * Mail Server${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "Mail Server"';
     else
-        echo -e "${green}Mail Server - Pass${NC}";
+        echo -e "${green}[PASS] * Mail Server${NC}";
     fi
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "GNOME Desktop Environment" > /dev/null`; then
-        echo -e "${red}GNOME Desktop Environment - Fail${NC}";
+        echo -e "${red}[FAIL] * GNOME Desktop Environment${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "GNOME Desktop Environment"';
     else
-        echo -e "${green}GNOME Desktop Environment - Pass${NC}";
+        echo -e "${green}[PASS] * GNOME Desktop Environment${NC}";
     fi
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "KDE (K Desktop Environment)" > /dev/null` ; then
-        echo -e "${red}KDE (K Desktop Environment) - Fail${NC}";
+        echo -e "${red}[FAIL] * KDE (K Desktop Environment)${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "KDE (K Desktop Environment)"';
     else
-        echo -e "${green}KDE (K Desktop Environment) - Pass${NC}";
+        echo -e "${green}[PASS] * KDE (K Desktop Environment)${NC}";
     fi
 
     if `yum grouplist | awk '/Installed Groups:/ {flag=1;next} /Available Groups:/{flag=0} flag {print}' | grep "Mono" > /dev/null` ; then
-        echo -e "${red}Mono - Fail${NC}";
+        echo -e "${red}[FAIL] * Mono${NC}";
         echo -e '\t \_ To remove this run: yum groupremove "Mono"';
     else
-        echo -e "${green}Mono - Pass${NC}";
+        echo -e "${green}[PASS] * Mono${NC}";
     fi
 
 fi
 
 # FQDN Check
-
-echo -e "${yellow}=====HOSTNAME CHECK=====${NC}"
+echo -e "====== HOSTNAME CHECK ======";
 
 hostname=`hostname`
 {
@@ -177,9 +209,9 @@ fi
 fqdnhost=`hostname | grep -P '[a-zA-Z0-9]+[.]+[a-zA-Z0-9]+[.]+[a-zA-Z0-9]'`
 
 if [[ $hostname = $fqdnhost ]]; then
-echo -e "${green}The server's hostname of $hostname is a FQDN${NC}"
+echo -e "${green}[PASS] * The server's hostname of $hostname is a FQDN${NC}"
 else
-echo -e "${red}The server's hostname of $hostname is not a FQDN${NC}"
+echo -e "${red}[FAIL] * The server's hostname of $hostname is not a FQDN${NC}"
 echo -e "\t \_ To fix this edit /etc/sysconfig/network and replaced the hostname on the HOSTNAME= line and reboot the server"
 echo -e "\t \_ For example a FQDN should look like server.domain.com or server.domain.co.uk"
 fi
@@ -191,53 +223,52 @@ if dig < /dev/null > /dev/null 2>&1  ; then
 
         if [ ! -z "$digresult" ]; then
             if [ $digresult == $hostnameip ]; then
-                echo -e "${green}The IP the hostname resolves to is the same as what's set on the server${NC}"
+                echo -e "${green}[PASS] * The IP the hostname resolves to is the same as what's set on the server${NC}"
             else
-                echo -e "${red}The hostname resolves to a different IP than what's set on the server${NC}"
-                echo -e "\t \_ ${red}The hostname should resolve to $hostnameip, but actually resolves to $digresult${NC}"
+                echo -e "${yellow}[WARN] * The hostname resolves to a different IP than what's set on the server${NC}"
+                echo -e "\t \_ The hostname should resolve to $hostnameip, but actually resolves to $digresult${NC}"
                 echo -e "\t \_ To fix this edit the A record in DNS for the hostname and point it to this servers primary IP $hostnameip" 
             fi
         else
-            echo -e "${red}The hostname on the server does not resolve to an IP address${NC}"
+            echo -e "${yellow}[WARN] * The hostname on the server does not resolve to an IP address${NC}"
             echo -e "\t \_ To fix this add an A record to DNS for the hostname and point it to the servers primary IP $hostnameip"
         fi
     else
-        echo -e "${red}The server's hostname is not in /etc/hosts!${NC}"
+        echo -e "${yellow}[WARN] * The server's hostname is not in /etc/hosts!${NC}"
         echo -e "\t \_ To fix this edit /etc/hosts and use the following example below, replacing server.domain.com with your hostname"
         echo -e "\t \_ $hostnameip server.domain.com"
     fi
 else
-    echo -e "${red}Can't check if hostname resolves to an IP because Dig is not installed.${NC}"
+    echo -e "${yellow}[WARN] * Can't check if hostname resolves to an IP because Dig is not installed.${NC}"
     echo -e "\t \_ To install dig run: yum install bind-utils"
 fi
 
 # OS & Kernel Check
-
-echo -e "${yellow}=====OS & KERNEL CHECK=====${NC}"
+echo -e "====== OS & KERNEL CHECK ======";
 
 if `grep "release 5.*" /etc/redhat-release > /dev/null`  ; then
-    echo -e "${green}The OS is Supported${NC}";
+    echo -e "${green}[PASS] * The OS is Supported${NC}";
     echo -e "\t \_ `cat /etc/redhat-release`"
 elif `grep "release 6.*" /etc/redhat-release > /dev/null`  ; then
-    echo -e "${green}The OS is Supported${NC}";
+    echo -e "${green}[PASS] * The OS is Supported${NC}";
     echo -e "\t \_ `cat /etc/redhat-release`"
 else
-    echo -e "${red}The OS is Not Supported${NC}";
+    echo -e "${red}[FAIL] * The OS is Not Supported${NC}";
     echo -e "\t \_ `cat /etc/redhat-release`"
 fi
 
 if `uname -r | grep "grs" > /dev/null`; then
-    echo -e "${red}Kernel Not Supported${NC}";
+    echo -e "${red}[FAIL] * Kernel Not Supported${NC}";
     echo -e "\t \_ GRSEC Kernels are Not Supported";
     echo -e "\t \_ `uname -r`";
 elif `uname -r | grep -i "xx" > /dev/null`; then
-    echo -e "${red}Kernel Not Supported${NC}";
+    echo -e "${red}[FAIL] * Kernel Not Supported${NC}";
     echo -e "\t \_ GRSEC Kernels are Not Supported";
     echo -e "\t \_ `uname -r`";
 elif `uname -r | grep -P "2.[0-9]." > /dev/null` ; then
-    echo -e "${green}Kernel Supported${NC}";
+    echo -e "${green}[PASS] * Kernel Supported${NC}";
     echo -e "\t \_ `uname -r`";
 else
-    echo -e "${red}Kernel Not Supported${NC}";
+    echo -e "${red}[FAIL] * Kernel Not Supported${NC}";
     echo -e "\t \_ `uname -r`";
 fi
